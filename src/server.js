32e5467714d263e406a2864a8badee4f47a078cc
Tiago ramos <tiago.ramos@overlabs.com>
2026-01-20 14:55:57 -0300

    mais uma vezz

--- src/server.js
+++ src/server.js
@@ -18,5 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível dificil http://localhost:8000")
+  console.log("API disponível mais uma vez http://localhost:8000")
 })
